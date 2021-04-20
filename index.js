4ca1119f83aa1ca9de0c98a0c097c1b7695dfd83
twinh <twinhuang@qq.com>
2021-04-20 10:02:24 +0800

    feat: 增加 `external-link` 和 `popup` 图标

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {createFromIconfontCN} from '@ant-design/icons';
 
 const Icon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1853242_jbyms0i6nfl.js',
+  scriptUrl: '//at.alicdn.com/t/font_1853242_kycaxknpff.js',
 });
 
 export default Icon;
